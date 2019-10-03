cf708b67d690e146bc222c1542d42e1db0e1e1e0
Joji Mathew <jojimathew.mec@gmail.com>
2019-10-03 11:22:47 +0530

    Updates

---
+++ precache-manifest.0cfc5c2f4d287005b5c3532cf204c6b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cb4d5abe9e4c1bea2de",
+    "url": "/draft-js-dnd/static/js/runtime~main.8cb4d5ab.js"
+  },
+  {
+    "revision": "55a539c4af34d3da7fd7",
+    "url": "/draft-js-dnd/static/js/main.55a539c4.chunk.js"
+  },
+  {
+    "revision": "2e4a7ecbdd3788eee50c",
+    "url": "/draft-js-dnd/static/js/1.2e4a7ecb.chunk.js"
+  },
+  {
+    "revision": "55a539c4af34d3da7fd7",
+    "url": "/draft-js-dnd/static/css/main.2867c62e.chunk.css"
+  },
+  {
+    "revision": "2e4a7ecbdd3788eee50c",
+    "url": "/draft-js-dnd/static/css/1.77b25476.chunk.css"
+  },
+  {
+    "revision": "f2add7d8ad0c8ca64ab6c317b5c7d06b",
+    "url": "/draft-js-dnd/index.html"
+  }
+];
